6de3d3ada4c7ba309f974983001e8f3f37e22661
Vitor <bgvitor@gmail.com>
2023-12-18 18:10:48 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let menumobile = document.querySelector('.mobilemenuicon');
-            if (menumobile.classList.contains('open')) {
-                menumobile.classList.remove('open');
-                document.querySelector('.iconmenu').src = "pictures/ic_menu-big.png";
-            }
-            else {
-                menumobile.classList.add('open');
-                document.querySelector('.iconmenu').src = "pictures/ic_close.png";
-            }
